f3fc241a7e78146e36a00c95d6b8160ea2dcacd1
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-07-16 14:21:30 -0700

    Build: 13 Branch: master  0.0.1-alpha SHA: 3a73373

--- src/viewState.js
+++ src/viewState.js
@@ -2,18 +2,7 @@
 (function() {
     "use strict";
         // Set common strings as variables for IDE code completion
-    var STATES = "states",
-        CURRENT_STATE = "currentState",
-        FUNCTION = "function",
-        ON_BEGIN = "onBegin",
-        ON_ENTER = "onEnter",
-        ON_EXIT = "onExit",
-        ON_FINISH = "onFinish",
-        ON_METHOD_NOT_HANDLED = "onMethodNotHandled",
-        ON_TRANSITION_NOT_HANDLED = "onTransitionNotHandled",
-        TRANSITIONING = "transitioning",
-        TRANSITION = "transition",
-        BBSM = Backbone.View.extend({
+    var BBSM = Backbone.View.extend({
 
             stateModel: undefined,
             states: undefined,
@@ -23,7 +12,18 @@
             getState: getState,
             transition: transition,
             getAllowedTransitions: getAllowedTransitions
-        });
+        }),
+        STATES = "states",
+        CURRENT_STATE = "currentState",
+        FUNCTION = "function",
+        ON_BEGIN = "onBegin",
+        ON_ENTER = "onEnter",
+        ON_EXIT = "onExit",
+        ON_FINISH = "onFinish",
+        ON_METHOD_NOT_HANDLED = "onMethodNotHandled",
+        ON_TRANSITION_NOT_HANDLED = "onTransitionNotHandled",
+        TRANSITIONING = "transitioning",
+        TRANSITION = "transition";
 
     function initialize() {
         var states = this.options.states,
@@ -32,8 +32,8 @@
         this.stateModel = new Backbone.Model();
         this.stateModel.set(STATES, _.keys(states));
         this.states = this.options.states;
-        this.listenTo(this.stateModel, "change:" + CURRENT_STATE, currentStateChanged.bind(this));
-        setupListeners.call(this, listeners);
+        this.listenTo(this.stateModel, "change:" + CURRENT_STATE, _currentStateChanged.bind(this));
+        _setupListeners.call(this, listeners);
         if (this.options.initialState) {
             this.transition(this.options.initialState);
         }
@@ -50,12 +50,12 @@
     function transition(newState) {
         var currentState = this.stateModel.get(CURRENT_STATE);
         if (!currentState || _.contains(this.states[currentState].allowedTransitions, newState)) {
-            transitionTo.call(this, {
+            _transitionTo.call(this, {
                 current: currentState,
                 next: newState
             });
         } else {
-            failTransition.call(this, newState);
+            _failTransition.call(this, newState);
         }
     }
 
@@ -63,46 +63,50 @@
         return this.states[state].allowedTransitions;
     };
 
-    // Private methods -------------------------------------------------------------------------------------------------
-    function setupListeners(listeners) {
+    // +---------------------------------------------------------------------------------------------------------------+
+    // ++-------------------------------------------------------------------------------------------------------------++
+    // ||  Private methods: since these are local, these must be called with "apply" or "call" to attach the context  ||
+    // ++-------------------------------------------------------------------------------------------------------------++
+    // +---------------------------------------------------------------------------------------------------------------+
+    function _setupListeners(listeners) {
         var self = this;
         _.forEach(listeners, function(oneListener) {
             _.forEach(self.options.eventListeners[oneListener], function(listener) {
                 self.listenTo(self, oneListener, listener.bind(self));
             });
         });
-        this.listenTo(this, ON_ENTER, callOnEnterOfState);
-        this.listenTo(this, ON_EXIT, callOnExitOfState);
+        this.listenTo(this, ON_ENTER, _callOnEnterOfState);
+        this.listenTo(this, ON_EXIT, _callOnExitOfState);
     }
 
-    function currentStateChanged(model, currentState) {
+    function _currentStateChanged(model, currentState) {
         // args for old state, new state
         this.trigger(TRANSITION, {
             previous: model.previous(CURRENT_STATE),
             current: currentState});
     }
 
-    function transitionTo(states) {
+    function _transitionTo(states) {
         this.trigger(ON_BEGIN, TRANSITIONING);
         this.trigger(ON_EXIT, states.current);
-        detachStateMethods.call(this, states.current);
-        attachStateMethods.call(this, states.next);
+        _detachStateMethods.call(this, states.current);
+        _attachStateMethods.call(this, states.next);
         this.stateModel.set(CURRENT_STATE, states.next);
         this.trigger(ON_ENTER, states.next);
         this.trigger(ON_FINISH, TRANSITIONING);
     }
 
-    function failTransition(failedState) {
+    function _failTransition(failedState) {
         this.trigger(ON_TRANSITION_NOT_HANDLED, failedState);
     }
 
     // Remove methods available in 'state' and replace them with noops
-    function detachStateMethods(state) {
+    function _detachStateMethods(state) {
 
     }
 
     // Attach method available in 'state'
-    function attachStateMethods(state) {
+    function _attachStateMethods(state) {
         var states = this.states[state];
         _.forEach(_.keys(states), function(stateMethod) {
             if (typeof states[stateMethod] === FUNCTION) {
@@ -111,13 +115,13 @@
         }.bind(this));
     }
 
-    function callOnEnterOfState() {
+    function _callOnEnterOfState() {
         if (this.onEnter) {
             this.onEnter();
         }
     }
 
-    function callOnExitOfState() {
+    function _callOnExitOfState() {
         if (this.onExit) {
             this.onExit();
         }
