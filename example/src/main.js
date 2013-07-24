d524a56c5d4da3bdae1200803b9d776d480adcbb
Peter Ajtai <peter.ajtai@thinksolid.com>
2013-07-24 08:33:24 -0700

    Build: 18 Branch: master  0.0.4 SHA: c01359c

---
+++ example/src/main.js
@@ -0,0 +1,77 @@
+/*global require*/
+'use strict';
+
+// Require.js allows us to configure shortcut alias
+require.config({
+    // The shim config allows us to configure dependencies for
+    // scripts that do not call define() to register a module
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        bbsm: {
+            deps: [
+                'backbone'
+            ]
+        }
+    },
+    paths: {
+        jquery: '../../components/jquery/jquery',
+        underscore: '../../components/underscore/underscore',
+        backbone: '../../components/backbone/backbone',
+        bbsm: '../../src/bbsm',
+        text: '../../components/requirejs-text/text',
+        elevator: 'views/elevator'
+    }
+});
+
+require([
+    'elevator'
+], function (Elevator) {
+
+    var WAITING_WITH_DOORS_OPEN = "waitingWithDoorsOpen",
+        DOORS_CLOSING = "doorsClosing",
+        DOORS_OPENING = "doorsOpening",
+        MOVING_UP = "movingUp",
+        MOVING_DOWN = "movingDown",
+        elevator = new Elevator({
+        initialState: "waitingWithDoorsOpen",
+        states: {
+            waitingWithDoorsOpen: {
+                allowedTransitions: [
+                    DOORS_CLOSING
+                ]
+            },
+            doorsClosing: {
+                allowedTransitions: [
+                    DOORS_OPENING, MOVING_UP, MOVING_DOWN
+                ]
+            },
+            doorsOpening: {
+                allowedTransitions: [
+                    WAITING_WITH_DOORS_OPEN
+                ]
+            },
+            movingUp: {
+                allowedTransitions: [
+                    DOORS_OPENING
+                ]
+            },
+            movingDown: {
+                allowedTransitions: [
+                    DOORS_OPENING
+                ]
+            }
+        }
+    });
+
+    elevator.start().render();
+
+});
